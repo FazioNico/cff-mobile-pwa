3fa59349326b35d46e08b9de9cdacaf1cb46505a
FazioNico <contact@nicolasfazio.ch>
2017-10-29 01:52:06 +0200

    update pipe & legs loop

--- src/pipes/moment/moment.ts
+++ src/pipes/moment/moment.ts
@@ -3,7 +3,7 @@
  * @Date:   27-10-2017
  * @Email:  contact@nicolasfazio.ch
  * @Last modified by:   webmaster-fazio
- * @Last modified time: 27-10-2017
+ * @Last modified time: 29-10-2017
  */
 
 import { Pipe, PipeTransform } from '@angular/core';
@@ -20,12 +20,11 @@ import * as moment from "moment";
 })
 export class MomentPipe implements PipeTransform {
   /**
-   * Takes a value and makes it lowercase.
+   * Takes a value and makes date/time format with moment.js.
    */
-  transform(value: string|number, arg:string, seconde:boolean= false):string {
+  transform(value: string|number, arg:string, seconde:boolean=false):string {
     if(seconde){
-      return moment.duration({'seconds' : +value})
-                   .minutes().toString()
+      return  moment.utc((+value)*1000).format(arg)
     }
     return moment(value).format(arg);
   }
