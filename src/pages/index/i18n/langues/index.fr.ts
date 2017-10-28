f6ea4f8a3793efb83db3bd12fa77dfc502c3aea1
FazioNico <contact@nicolasfazio.ch>
2017-10-28 12:29:18 +0200

    fix UI

--- src/pages/index/i18n/langues/index.fr.ts
+++ src/pages/index/i18n/langues/index.fr.ts
@@ -3,14 +3,14 @@
 * @Date:   21-10-2017
 * @Email:  contact@nicolasfazio.ch
  * @Last modified by:   webmaster-fazio
- * @Last modified time: 27-10-2017
+ * @Last modified time: 28-10-2017
 */
 
 export const locale = {
   "lang": "fr",
   "data": {
     "index": { // <= namespace for module/component translations
-      "title": "Rechercher un horaire",
+      "title": "Rechercher une correspondance",
       "form": {
         from: 'De',
         to: 'À',
