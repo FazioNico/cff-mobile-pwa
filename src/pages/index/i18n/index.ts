7077d1543f9d7b8f2422953e753ca5149d41291e
FazioNico <contact@nicolasfazio.ch>
2017-10-29 02:02:38 +0200

    add www folder

--- src/pages/index/i18n/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @Author: Nicolas Fazio <webmaster-fazio>
- * @Date:   27-10-2017
- * @Email:  contact@nicolasfazio.ch
- * @Last modified by:   webmaster-fazio
- * @Last modified time: 27-10-2017
- */
-
-export * from './index-i18n.module'
